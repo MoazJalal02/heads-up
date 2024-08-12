a2f5b9db2663c0297277f42062180b7118393156
Muaz02 <167197216+MoazJalal@users.noreply.github.com>
2024-08-12 12:24:03 +0800

    rebuild checkout page for mobiles

--- src/components/cart/cart/Cart.tsx
+++ src/components/cart/cart/Cart.tsx
@@ -1,12 +1,24 @@
 import styles from './cart.module.css'
 import CartItem from '../cartItem/CartItem'
-import type { shoppingCart } from '@/lib/types'
+import type { cartItem, ProductType, shoppingCart } from '@/lib/types'
+import Link from 'next/link'
 
 type CartProps = {
     cart: shoppingCart
 }
 
+const getDiscount = (cartItems: cartItem[]) => {
+    var totalDiscount = 0;
+    cartItems.forEach(item => {
+        if(item.product.discount !== 0.00) totalDiscount += item.product.discount * item.product.price
+    })
+
+    return totalDiscount
+} 
+
 export default function Cart({cart}:CartProps) {
+
+    const totalDiscount = getDiscount(cart.items)
     return (
         <>
             <div className={styles.cartHeader}>
@@ -34,20 +46,27 @@ export default function Cart({cart}:CartProps) {
                 </div>
                 <div className={styles.orderContainer}>
                     <h2>ORDER SUMMARY</h2>
-                    <div className={styles.subtotalContainer}>
-                        <p>Subtotal <span className={styles.midDarkGray}>{`( ${cart.size} ${cart.size == 1? 'item':'items'} )`}</span></p>
-                        <p>{cart.subtotal}</p>
+                    <div className={styles.discountSubtotalContainer}>
+                        <div className={styles.subtotalContainer}>
+                            <p>Subtotal <span className={styles.midDarkGray}>{`( ${cart.size} ${cart.size == 1? 'item':'items'} )`}</span></p>
+                            <p>${(Math.round(cart.subtotal * 100) / 100).toFixed(2)}</p>
+                        </div>
+                        <div className={styles.subtotalContainer}>
+                            <p>Discount</p>
+                            <p className={styles.red}>-${Math.round(totalDiscount).toFixed(2)}</p>
+                        </div>
                     </div>
-                    <hr></hr>
+                    <hr className={styles.divideLine}></hr>
                     <div className={styles.totalContainer}>
                         <h3>Order Total</h3>
-                        <div>
-                            <h3>{cart.subtotal}</h3>
+                        <div className={styles.totalContent}>
+                            <h3>${Math.round(cart.subtotal - totalDiscount).toFixed(2)}</h3>
                             <p className={styles.midDarkGray}>Inlusive of VAT</p>
                         </div>
                     </div>
                     <div className={styles.actionContainer}>
-                        <button className={styles.CheckoutBtn}>Checkout</button>
+                        <Link href='/top-deals' className={`${styles.link} ${styles.shoppingLink}`}><h2>Continue Shopping</h2></Link>
+                        <button className={styles.CheckoutBtn}><h2>Checkout</h2></button>
                     </div>
                 </div>
             </div>
